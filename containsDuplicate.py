f8d3f9e8db38463e6d0d9dcc8cc9eb4ead4ff03c
Elevenv <vitthaljadhav1420@gmail.com>
2022-12-13 09:42:53 +0530

    Day 1

---
+++ containsDuplicate.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/contains-duplicate-ii/
+# 219. Contains Duplicates 
+
+nums = [1,2,3,1,2,3]
+# nums = [1,2,3,1]
+# nums = [1,0,1,1]
+k = 2
+d = {}
+for i in range(len(nums)):
+    if nums[i] in d:
+        if abs(d[nums[i]]-i)<=k:
+            print(True)
+            break
+        else:
+            d[nums[i]] = i
+    else:
+        d[nums[i]] = i
+else:
+    print(False)
+
+
+s = set(nums)
+if len(s)==len(nums):
+    print(False)
+else:
+    print(True)
