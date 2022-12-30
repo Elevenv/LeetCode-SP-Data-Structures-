ffef0db06d1c33b523712d6403071f60f4627507
Elevenv <vitthaljadhav1420@gmail.com>
2022-12-30 14:19:22 +0530

    Day 11

---
+++ BTLevelOrderTrav.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue = collections.deque()
+        res = []
+
+        queue.append(root)
+
+        while queue:
+            qLen = len(queue)
+            level = []
+
+            for i in range(qLen):
+                node = queue.popleft()
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            
+            if level:
+                res.append(level)
+        return res
+        
